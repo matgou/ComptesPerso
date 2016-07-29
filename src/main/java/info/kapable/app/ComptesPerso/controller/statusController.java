779f939c56e45e1455b07de57c5e6030935150f5
Johanna <ptitedjo@gmail.com>
2016-07-29 13:56:33 +0200

    Commit initial

---
+++ src/main/java/info/kapable/app/ComptesPerso/controller/statusController.java
@@ -0,0 +1,29 @@
+package info.kapable.app.ComptesPerso.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+ 
+@Controller
+@RequestMapping("/")
+/**
+ * Simple controller to handle monitoring request
+ * @author Johanna
+ *
+ */
+public class statusController {
+ 
+    @RequestMapping(method = RequestMethod.GET)
+    public String sayHello(ModelMap model) {
+        model.addAttribute("greeting", "Hello World from Spring 4 MVC");
+        return "monitoring";
+    }
+ 
+    @RequestMapping(value="/helloagain", method = RequestMethod.GET)
+    public String sayHelloAgain(ModelMap model) {
+        model.addAttribute("greeting", "Hello World Again, from Spring 4 MVC");
+        return "monitoring";
+    }
+}
