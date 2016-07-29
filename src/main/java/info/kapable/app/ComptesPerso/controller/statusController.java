76c4ffa441a5eb7f8e429ab8f77e6a721f36cb34
Mathieu GOULIN <mathieu.goulin@gadz.org>
2016-07-29 15:12:16 +0200

    add status action /status/ /status/ping and /status/time

--- src/main/java/info/kapable/app/ComptesPerso/controller/statusController.java
+++ src/main/java/info/kapable/app/ComptesPerso/controller/statusController.java
@@ -5,9 +5,12 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
  
 @Controller
-@RequestMapping("/")
+@RequestMapping("/status")
 /**
  * Simple controller to handle monitoring request
  * @author Johanna
@@ -16,14 +19,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class statusController {
  
     @RequestMapping(method = RequestMethod.GET)
-    public String sayHello(ModelMap model) {
-        model.addAttribute("greeting", "Hello World from Spring 4 MVC");
+    public String sayStatus(ModelMap model) {
+        model.addAttribute("greeting", "Application ComptesPerso is OK");
         return "monitoring";
     }
  
-    @RequestMapping(value="/helloagain", method = RequestMethod.GET)
-    public String sayHelloAgain(ModelMap model) {
-        model.addAttribute("greeting", "Hello World Again, from Spring 4 MVC");
+    @RequestMapping(value="/ping", method = RequestMethod.GET)
+    public String sayPong(ModelMap model) {
+        model.addAttribute("greeting", "pong");
+        return "monitoring";
+    }
+
+    @RequestMapping(value="/time", method = RequestMethod.GET)
+    public String sayTime(ModelMap model) {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        model.addAttribute("greeting", sdf.format(cal.getTime()) );
         return "monitoring";
     }
 }
