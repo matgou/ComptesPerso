8f7f651f5fa6e537260791d8be759829ae181c68
Mathieu GOULIN <mathieu.goulin@gadz.org>
2016-08-01 20:56:00 +0200

    Fix configuration for webapp

--- src/main/java/info/kapable/app/ComptesPerso/service/FoyerServiceImpl.java
+++ src/main/java/info/kapable/app/ComptesPerso/service/FoyerServiceImpl.java
@@ -11,24 +11,24 @@ import info.kapable.app.ComptesPerso.pojo.Foyer;
 @Service
 public class FoyerServiceImpl implements FoyerService {
 
-	   @Autowired
-	   protected FoyerDAO foyerDAO;
-	   
-	   public Foyer newFoyer(String name) {
-		   Foyer foyer = new Foyer(name);
-		   this.foyerDAO.save(foyer);
-		   return foyer;
-	   }
+	@Autowired
+	protected FoyerDAO foyerDAO;
+
+	public Foyer newFoyer(String name) {
+		Foyer foyer = new Foyer(name);
+		this.foyerDAO.save(foyer);
+		return foyer;
+	}
 
 	@Override
 	public Foyer getFoyer(Long id) {
-		
+
 		return this.foyerDAO.get(id);
 	}
 
 	@Override
 	public void updateFoyer(Foyer foyer) {
 		this.foyerDAO.save(foyer);
-		
+
 	}
 }
