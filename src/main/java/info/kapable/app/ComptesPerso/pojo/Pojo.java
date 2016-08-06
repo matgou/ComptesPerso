e4d0f48eba77512d9ae8e7e6d34e49e20f4de7d8
Mathieu Goulin <Mathieu Goulin>
2016-08-06 22:57:00 +0200

    Defaut Pojo

---
+++ src/main/java/info/kapable/app/ComptesPerso/pojo/Pojo.java
@@ -0,0 +1,10 @@
+package info.kapable.app.ComptesPerso.pojo;
+
+public abstract class Pojo implements Cloneable {
+
+	public abstract Long getId();
+
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
