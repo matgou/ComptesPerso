737bc2526032312a12f1a4fa90d9fa3b7a0a6021
Mathieu GOULIN <mathieu.goulin@gadz.org>
2016-09-18 18:00:11 +0200

    Gestion des exceptions

--- src/main/java/info/kapable/app/ComptesPerso/controller/AppExceptionHandler.java
+++ src/main/java/info/kapable/app/ComptesPerso/controller/AppExceptionHandler.java
@@ -11,52 +11,51 @@ import javax.validation.ConstraintViolationException;
 
 import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
-import org.springframework.transaction.TransactionSystemException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import info.kapable.app.ComptesPerso.pojo.ErrorResource;
 
-
 @ControllerAdvice
-public class AppExceptionHandler extends ResponseEntityExceptionHandler {
+public class AppExceptionHandler {
 
 	private static Logger logger = Logger.getLogger(AppExceptionHandler.class);
-	
-	private Throwable getCause(Throwable e) {
-	    Throwable cause = null; 
-	    Throwable result = e;
 
-	    while(null != (cause = result.getCause())  && (result != cause) ) {
-	        result = cause;
-	    }
-	    return result;
+	private Throwable getCause(Throwable e) {
+		Throwable cause = null;
+		Throwable result = e;
+
+		while (null != (cause = result.getCause()) && (result != cause)) {
+			result = cause;
+		}
+		return result;
 	}
-	
-    @ExceptionHandler(TransactionSystemException.class)
+
+	@ExceptionHandler(Exception.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ResponseBody
-    protected ErrorResource handleInvalidRequest(TransactionSystemException ex) {
-        Throwable t = this.getCause(ex);
-        logger.error("Server error", ex);
-        if(t instanceof ConstraintViolationException){
-        	ConstraintViolationException cve = (ConstraintViolationException) t;
-           Set<ConstraintViolation<?>> constraints = cve.getConstraintViolations();
-           Map<String,List<String>> constraintList = new HashMap<String,List<String>>(); 
-           for(ConstraintViolation<?> constraint: constraints) {
-        	   String message = constraint.getMessage();
-        	   String property = constraint.getPropertyPath().toString();
-        	   if(!constraintList.containsKey(property)) {
-        		   constraintList.put(property, new ArrayList<String>());
-        	   }
-        	   constraintList.get(property).add(message);
-           }
-           return new ErrorResource("InvalidEntity", t.getMessage(), constraintList);    
-        }else {
-           return new ErrorResource("Unknow server error", t.getMessage());
-        }
-    }
-} 
+	public ErrorResource myError(Exception ex) {
+		logger.error("A exception has been trow");
+
+		Throwable t = this.getCause(ex);
+		logger.error("Server error", ex);
+		if (t instanceof ConstraintViolationException) {
+			ConstraintViolationException cve = (ConstraintViolationException) t;
+			Set<ConstraintViolation<?>> constraints = cve.getConstraintViolations();
+			Map<String, List<String>> constraintList = new HashMap<String, List<String>>();
+			for (ConstraintViolation<?> constraint : constraints) {
+				String message = constraint.getMessage();
+				String property = constraint.getPropertyPath().toString();
+				if (!constraintList.containsKey(property)) {
+					constraintList.put(property, new ArrayList<String>());
+				}
+				constraintList.get(property).add(message);
+			}
+			return new ErrorResource("InvalidEntity", t.getMessage(), constraintList);
+		} else {
+			return new ErrorResource("Unknow server error", t.getMessage());
+		}
+	}
+}
--- src/main/webapp/js/extensions/categoryTreeHandler.js
+++ src/main/webapp/js/extensions/categoryTreeHandler.js
@@ -3,7 +3,5 @@
  * This service is to access on Account business ressources via rest
  */
 function categoryTreeHandler(response) {
-	console.log("Remise en forme de la category: ");
-	console.log(response.data);
 	return response;
 }
